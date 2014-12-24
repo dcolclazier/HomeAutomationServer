9d9446b1273879bb85b77ad02b482c7be839a4e2
dcolclazier <jdcolclazier@gmail.com>
2014-12-24 03:40:49 -0700

    Initial commit - server software

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HomeAutomationServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richText_Status = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richText_Status
+            // 
+            this.richText_Status.Location = new System.Drawing.Point(910, 451);
+            this.richText_Status.Name = "richText_Status";
+            this.richText_Status.Size = new System.Drawing.Size(348, 284);
+            this.richText_Status.TabIndex = 0;
+            this.richText_Status.Text = "";
+            this.richText_Status.TextChanged += new System.EventHandler(this.richText_Status_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 744);
+            this.Controls.Add(this.richText_Status);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richText_Status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeAutomationServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richText_Status_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+       
+        
+
+        
+
+        public void WriteToBox(string text)
+        {
+            richText_Status.AppendText(text);
+        }
+
+        public void ClearTextBox()
+        {
+            richText_Status.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            var server = new Server(this, 420);
+
+        }
+    }
+    public static class CrossThreadExtensions
+        {
+            public static void PerformSafely(this Control target, Action action)
+            {
+                if (target.InvokeRequired)
+                    target.Invoke(action);
+                else
+                    action();
+            }
+            public static void PerformSafely<T1>(this Control target, Action<T1> action, T1 parameter)
+            {
+                if (target.InvokeRequired)
+                    target.Invoke(action, parameter);
+                else
+                    action(parameter);
+            }
+            public static void PerformSafely<T1, T2>(this Control target, Action<T1, T2> action, T1 p1, T2 p2)
+            {
+                if (target.InvokeRequired)
+                    target.Invoke(action, p1, p2);
+                else
+                    action(p1, p2);
+            }
+        }
+    public class Server
+        {
+            private readonly TcpListener _tcpListener;
+            private readonly Form1 _form;
+            private readonly TcpListener _helloListener;
+
+            public Server(Form1 form, int listeningPort)
+            {
+                _form = form;
+                _tcpListener = new TcpListener(IPAddress.Parse("192.168.168.101"), listeningPort);
+                _helloListener = new TcpListener(IPAddress.Parse("192.168.168.101"), 59400);
+
+                var listenThread = new Thread(ListenForConnection);
+                var helloThread = new Thread(ListenForConnection);
+                _form.WriteToBox("It's working!\n");
+                listenThread.Start(_tcpListener);
+                helloThread.Start(_helloListener);
+            }
+
+            private void ListenForConnection(object obj)
+            {
+                var listener = (TcpListener)obj;
+                listener.Start();
+                while (true)
+                {
+                    var client = listener.AcceptTcpClient();
+                    var clientThread = new Thread(HandleClientComm);
+                    clientThread.Start(client);
+                }
+            }
+            private void ListenForClients()
+            {
+                _tcpListener.Start();
+                while (true)
+                {
+                    var client = _tcpListener.AcceptTcpClient();
+                    var clientThread = new Thread(HandleClientComm);
+                    clientThread.Start(client);
+                }
+            }
+
+            private void HandleClientComm(object client) //bug - refactor the shit out of this... i hate it - its ugly.
+            {
+
+                var tcpClient = (TcpClient)client;
+                var encoder = new UTF8Encoding();
+                var clientStream = tcpClient.GetStream();
+
+
+                var buffer = encoder.GetBytes("Hello!");
+                clientStream.Write(buffer, 0, buffer.Length);
+                clientStream.Flush();
+
+
+                var message = new byte[4096];
+                int bytesRead;
+                while (true)
+                {
+                    bytesRead = 0;
+                    try
+                    {
+                        bytesRead = clientStream.Read(message, 0, 4096);
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+                    ProcessMessage(message, bytesRead, clientStream);
+                    //_form.PerformSafely(()=>_form.ClearTextBox());
+                    var read = bytesRead;
+                    _form.PerformSafely(() => _form.WriteToBox(encoder.GetString(message, 0, read) + "\n"));
+                }
+
+
+                tcpClient.Close();
+            }
+
+            private void ProcessMessage(byte[] buffer, int bytesRead, Stream stream)
+            {
+                var encoder = new UTF8Encoding();
+                var message = encoder.GetString(buffer, 0, bytesRead);
+
+                if (message == "Hello?")
+                {
+                    Debug.Print("Got a 'Hello?'   !!!");
+                    var sendBuffer = encoder.GetBytes("Hello!");
+                    stream.Write(sendBuffer, 0, sendBuffer.Length);
+                }
+
+            }
+
+
+
+
+        }
+    
+}
